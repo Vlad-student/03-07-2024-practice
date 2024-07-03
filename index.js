8d80378670190df11f2fbb5b609bfcde9538bf4e
Vlad-student <vlad.v.terletskiy@gmail.com>
2024-07-03 17:11:37 +0300

    Arrow practice

---
+++ index.js
@@ -0,0 +1,24 @@
+// const numbers = [5, 10, 15, 20, 25, 30, 35];
+
+console.group('1 task')
+// const numbers = [5, 10, 15, 20, 25, 30, 35];
+// console.log(numbers);
+// const lastItem=numbers.pop();
+// numbers.unshift(lastItem);
+// console.log(numbers);
+console.groupEnd();
+
+console.group('2 task')
+// const numbers = [5, 10, 15, 20, 25, 30, 35];
+// console.log(numbers);
+// const firstItem=numbers.shift();
+// numbers.push(firstItem);
+// console.log(numbers);
+console.groupEnd();
+
+console.group('3 task')
+
+
+
+
+console.groupEnd();
